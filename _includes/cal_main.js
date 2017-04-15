de4edf2ce44da367ff1aef167475e7c04aae6439
Leonid Petrov <lenia.petrov@gmail.com>
2017-04-15 09:28:58 -0400

    this all works great, now I need to implement the display of events

--- _includes/cal_main.js
+++ _includes/cal_main.js
@@ -1,5 +1,6 @@
 <script>
-  var apiKey = 'AIzaSyA7Uka7Cbx7SPTWqDn52Nw9XPAe1kdQZxs';
+// javascript to access all seminar google calendars which puts them onto main page;
+// its modifications can be used for seminar pages
   var userEmail = [
     "dd0lvqfa6j2vtbocbhnsp3u380@group.calendar.google.com",
     "6njs6bnklu56g6lhi5ojl2pha8@group.calendar.google.com",
@@ -11,31 +12,29 @@
     "f0un05c36pdv08n0m90bi99jmk@group.calendar.google.com",
     "pce8r0mnja2do20vkku2gslamk@group.calendar.google.com",
     "starrie@virginia.edu"
-  ]; //your calendar Ids
+  ]; //list of all calendars, new seminar google calendars can be added here
+  var apiKey = 'AIzaSyA7Uka7Cbx7SPTWqDn52Nw9XPAe1kdQZxs';
   var clientId = '924411057957-0d9mrr6c8uvgsbdq1v1he5ha0je1om3d.apps.googleusercontent.com';
-  var userTimeZone = "New_York"; //example "Rome" "Los_Angeles" ecc...
-  var maxRows = 3; //events to shown
   var scopes = 'https://www.googleapis.com/auth/calendar.readonly';
+  // google API keys
+  var userTimeZone = "New_York"; // Charlottesville is in this timezone so we keep it like this
+  var maxRows = 10;
+
+  var propertySeparator = "__sep__";
 
   var eventsArray = [];
+  var calsArray = [];
 
-  //--------------------- Add a 0 to numbers
   function padNum(num) {
       if (num <= 9) {
           return "0" + num;
       }
       return num;
   }
-  //--------------------- end
-
-  //--------------------- From 24h to Am/Pm
   function AmPm(num) {
       if (num <= 12) { return "am " + num; }
       return "pm " + padNum(num - 12);
   }
-  //--------------------- end
-
-  //--------------------- num Month to String
   function monthString(num) {
            if (num === "01") { return "Jan"; }
       else if (num === "02") { return "Feb"; }
@@ -50,9 +49,6 @@
       else if (num === "11") { return "Nov"; }
       else if (num === "12") { return "Dec"; }
   }
-  //--------------------- end
-
-  //--------------------- from num to day of week
   function dayString(num){
            if (num == "1") { return "Mon" }
       else if (num == "2") { return "Tue" }
@@ -62,37 +58,26 @@
       else if (num == "6") { return "Sat" }
       else if (num == "0") { return "Sun" }
   }
-  //--------------------- end
-
-  //--------------------- client CALL
   function handleClientLoad() {
       gapi.client.setApiKey(apiKey);
       checkAuth();
   }
-  //--------------------- end
-
-  //--------------------- check Auth
   function checkAuth() {
       gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
   }
-  //--------------------- end
-
-  //--------------------- handle result and make CALL
   function handleAuthResult(authResult) {
       if (authResult) {
           makeApiCall();
       }
   }
-  //--------------------- end
-
   function appendPre(message) {
+    //prints in a bare way
     var pre = document.getElementById('content');
     var textContent = document.createTextNode(message + '\n');
     pre.appendChild(textContent);
   }
 
-  //--------------------- API CALL itself
-
+  //--------------------- main function makes API calls and displays results
   function makeApiCall(callback) {
     var executeOnce = 0;
     var today = new Date();
@@ -107,27 +92,32 @@
           'timeMin': today.toISOString(),
           'maxResults': maxRows,
           'orderBy': 'startTime'});
-        request.execute(function (resp) {
+        request.execute(function (resp)
+        {
+          calsArray.push('a');
           for (var i = 0; i < resp.items.length; i++) {
             var item = resp.items[i];
             var allDay = item.start.date? true : false;
             var startDT = allDay ? item.start.date : item.start.dateTime;
-            eventsArray.push(startDT + ' ' + item.summary);
-            // appendPre(startDT + ' ' + item.summary);
+            eventsArray.push(startDT + propertySeparator + item.summary);
+            // formatted google calendar events are put into array as strings
             }
-            if(eventsArray.length == 19 && !executeOnce)
+          if(calsArray.length == userEmail.length && !executeOnce)
+          {
+            eventsArray.sort();
+            // the array is sorted after all calendars are processes
+            for (var j = 0; j < eventsArray.length; j++)
             {
-              eventsArray.sort();
-              for (var j = 0; j < eventsArray.length; j++)
-              {
-                appendPre(j + '  ' + eventsArray[j]);
-              }
-              executeOnce = 1;
+              //this is where the events' representation happens
+              el = eventsArray[j];
+              appendPre(el.split(propertySeparator)[0]);
+              appendPre(el.split(propertySeparator)[1]);
+            }
+            executeOnce = 1;
           };
         });
       };
     });
-
   }
 </script>
 <script src='https://apis.google.com/js/client.js?onload=handleClientLoad'></script>
