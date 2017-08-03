8b4e64951f392a85dd1dc3b59c0b58490d9aca45
Leonid Petrov <lenia.petrov@gmail.com>
2017-08-03 09:52:06 +0500

    asked to update Lou's list and got a much better automatic listing

--- script/lou_course_descriptions.py
+++ script/lou_course_descriptions.py
@@ -35,37 +35,37 @@ print ""
 for i in range(1, len(data)):
     if i%2 :
         # renames and redo descriptions
-        if data[i][0].replace("MATH ", "", 1) == "4210":
-            print "- name: \"", "Mathematics for Physics (3.00)", "\""
-        elif data[i][0].replace("MATH ", "", 1) == "1190":
-            print "- name: \"", "A Survey of Calculus I with Algebra (4.00)", "\""
-        elif data[i][0].replace("MATH ", "", 1) == "1210":
-            print "- name: \"", " A survey of Calculus I (3.00)", "\""
-        elif data[i][0].replace("MATH ", "", 1) == "1220":
-            print "- name: \"", " A survey of Calculus II (3.00)", "\""
-        else :
-            print "- name: \"", data[i][1], "\""
+        # if data[i][0].replace("MATH ", "", 1) == "4210":
+        #     print "- name: \"", "Mathematics for Physics (3.00)", "\""
+        # elif data[i][0].replace("MATH ", "", 1) == "1190":
+        #     print "- name: \"", "A Survey of Calculus I with Algebra (4.00)", "\""
+        # elif data[i][0].replace("MATH ", "", 1) == "1210":
+        #     print "- name: \"", " A survey of Calculus I (3.00)", "\""
+        # elif data[i][0].replace("MATH ", "", 1) == "1220":
+        #     print "- name: \"", " A survey of Calculus II (3.00)", "\""
+        # else :
+        print "- name: \"", data[i][1], "\""
         print "  number:", data[i][0].replace("MATH ", "", 1)
         if int( data[i][0].replace("MATH ", "", 1) ) >= 5000 :
             print "  graduate: true"
     else :
         if data[i][0] == "Offered" + current_semester :
-            if data[i-1][0].replace("MATH ", "", 1) != "8700":
-                #somehow 8700 is offered on Lou's list but not in reality
-                print "  offered: " + current_semester
+            # if data[i-1][0].replace("MATH ", "", 1) != "8700":
+            #somehow 8700 is offered on Lou's list but not in reality
+            print "  offered: " + current_semester
             # redo descriptions
-            if data[i-1][0].replace("MATH ", "", 1) == "4900":
-                print "  descr: \"", "This course provides a framework for the completion of a Distinguished Major Thesis, a treatise containing an exposition of a chosen mathematical topic.  A faculty advisor guides a student through the beginning phases of the process of research and writing.  Prerequisite: Acceptance into the Distinguished Major Program.", "\""
-            elif data[i-1][0].replace("MATH ", "", 1) == "4901":
-                print "  descr: \"", "This is the second semester of a two semester sequence for the purpose of the completion of a Distinguished Major Thesis. A faculty member guides the student through all phases of the process which culminates in an open presentation of the thesis to an audience including a faculty evaluation committee.  Prerequisite: MATH 4900.", "\""
-            else:
-                print "  descr: \"", data[i][1].encode('utf-8').split("Course was offered", 1)[0], "\""
+            # if data[i-1][0].replace("MATH ", "", 1) == "4900":
+            #     print "  descr: \"", "This course provides a framework for the completion of a Distinguished Major Thesis, a treatise containing an exposition of a chosen mathematical topic.  A faculty advisor guides a student through the beginning phases of the process of research and writing.  Prerequisite: Acceptance into the Distinguished Major Program.", "\""
+            # elif data[i-1][0].replace("MATH ", "", 1) == "4901":
+            #     print "  descr: \"", "This is the second semester of a two semester sequence for the purpose of the completion of a Distinguished Major Thesis. A faculty member guides the student through all phases of the process which culminates in an open presentation of the thesis to an audience including a faculty evaluation committee.  Prerequisite: MATH 4900.", "\""
+            # else:
+            print "  descr: \"", data[i][1].encode('utf-8').split("Course was offered", 1)[0], "\""
         else:
             # redo descriptions
-            if data[i-1][0].replace("MATH ", "", 1) == "4900":
-                print "  descr: \"", "This course provides a framework for the completion of a Distinguished Major Thesis, a treatise containing an exposition of a chosen mathematical topic.  A faculty advisor guides a student through the beginning phases of the process of research and writing.  Prerequisite: Acceptance into the Distinguished Major Program.", "\""
-            elif data[i-1][0].replace("MATH ", "", 1) == "4901":
-                print "  descr: \"", "This is the second semester of a two semester sequence for the purpose of the completion of a Distinguished Major Thesis. A faculty member guides the student through all phases of the process which culminates in an open presentation of the thesis to an audience including a faculty evaluation committee.  Prerequisite: MATH 4900.", "\""
-            else:
-                print "  descr: \"", data[i][0].encode('utf-8').split("Course was offered", 1)[0], "\""
+            # if data[i-1][0].replace("MATH ", "", 1) == "4900":
+            #     print "  descr: \"", "This course provides a framework for the completion of a Distinguished Major Thesis, a treatise containing an exposition of a chosen mathematical topic.  A faculty advisor guides a student through the beginning phases of the process of research and writing.  Prerequisite: Acceptance into the Distinguished Major Program.", "\""
+            # elif data[i-1][0].replace("MATH ", "", 1) == "4901":
+                # print "  descr: \"", "This is the second semester of a two semester sequence for the purpose of the completion of a Distinguished Major Thesis. A faculty member guides the student through all phases of the process which culminates in an open presentation of the thesis to an audience including a faculty evaluation committee.  Prerequisite: MATH 4900.", "\""
+            # else:
+            print "  descr: \"", data[i][0].encode('utf-8').split("Course was offered", 1)[0], "\""
         print ""
