46da9efb6d1ede6db967194b92d350f3749cc16d
Leonid Petrov <lenia.petrov@gmail.com>
2017-08-03 15:07:48 +0500

    math physics seminar - trying to process

---
+++ seminars/mathphys/1999-03/dbpabs/images.pl
@@ -0,0 +1,48 @@
+# LaTeX2HTML 2K.1beta (1.48)
+# Associate images original text with physical files.
+
+
+$key = q/theta(D+1)=1+sigma(D);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="150" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$\theta(D+1)=1+\sigma(D)$">|; 
+
+$key = q/D;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="18" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$D$">|; 
+
+$key = q/D+1;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="45" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$D+1$">|; 
+
+$key = q/nu_{perp}(D+1)=nu(D);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="132" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$\nu_{\perp}(D+1)=\nu(D)$">|; 
+
+$key = q/sigma(D);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="40" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="$\sigma(D)$">|; 
+
+$key = q/N;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="19" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$N$">|; 
+
+$key = q/gamma(D+1)=alpha(D);MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="124" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$\gamma(D+1)=\alpha(D)$">|; 
+
+1;
+
