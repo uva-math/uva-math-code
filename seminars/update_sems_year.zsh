2eaf8851cf560ef182dc166c6faaed44fb03044e
Leonid Petrov <lenia.petrov@gmail.com>
2023-05-17 11:23:21 -0400

    w

--- seminars/update_sems_year.zsh
+++ seminars/update_sems_year.zsh
@@ -9,9 +9,13 @@ for dir in algebra colloq diffeq galois geometry gradsem mathclub mathphys ntsem
   cp "${dir}/${dir}22_23.html" "${dir}/${dir}23_24.html"
 
   # Update the archive line, title, layout, permalink, show_from and show_to for 2023-24 in the new file
-  sed -i '' -e "s#<a href=\"/seminars/${dir}/\">upcoming</a> | <a href=\"/seminars/${dir}/2022-23/\">2022-23</a>#<a href=\"/seminars/${dir}/\">upcoming</a> | <a href=\"/seminars/${dir}/2023-24/\">2023-24</a> | <a href=\"/seminars/${dir}/2022-23/\">2022-23</a>#g" \
-  -e 's#2022-23/#2023-24/#g' \
-  -e 's#2022-23\n#2023-24\r#g' \
+  sed -i '' \
+  -e 's#2022-23/$#2023-24/#g' \
+  -e 's#2022-23$#2023-24#g' \
   -e 's#show_from='\''1 July 2022'\''#show_from='\''1 July 2023'\''#g' \
   -e 's#show_to='\''1 July 2023'\''#show_to='\''1 July 2024'\''#g' "${dir}/${dir}23_24.html"
+
+  sed -i '' \
+  -e "s#<a href=\"/seminars/${dir}/\">upcoming</a> | <a href=\"/seminars/${dir}/2022-23/\">2022-23</a>#<a href=\"/seminars/${dir}/\">upcoming</a> | <a href=\"/seminars/${dir}/2023-24/\">2023-24</a> | <a href=\"/seminars/${dir}/2022-23/\">2022-23</a>#g" \
+  "${dir}/*.html"
 done
