c30db4ad4af511d50642f670f49c98b819aba8ee
Leonid Petrov <lenia.petrov@gmail.com>
2023-05-17 11:10:02 -0400

    Update udpate_sems_year.zsh

--- seminars/udpate_sems_year.zsh
+++ seminars/udpate_sems_year.zsh
@@ -8,12 +8,9 @@ for dir in algebra ancommons colloq diffeq galois geometry gradsem mathclub math
   # Copy content of the original file to the new one
   cp "${dir}/${dir}22_23.html" "${dir}/${dir}23_24.html"
 
-  # Find and replace the archive line in the new file
-  sed -i '' 's|<a href="/seminars/'${dir}'/">upcoming</a> | <a href="/seminars/'${dir}'/2022-23/">2022-23</a>|<a href="/seminars/'${dir}'/">upcoming</a> | <a href="/seminars/'${dir}'/2023-24/">2023-24</a> | <a href="/seminars/'${dir}'/2022-23/">2022-23</a>|' "${dir}/${dir}23_24.html"
-
-  # Update the title, layout, permalink, show_from and show_to for 2023-24 in the new file
-  sed -i '' 's|2022-23|2023-24|' "${dir}/${dir}23_24.html"
-  sed -i '' 's|permalink: /seminars/'${dir}'/2022-23/|permalink: /seminars/'${dir}'/2023-24/|' "${dir}/${dir}23_24.html"
-  sed -i '' 's|show_from='\''1 July 2022'\''|show_from='\''1 July 2023'\''|' "${dir}/${dir}23_24.html"
-  sed -i '' 's|show_to='\''1 July 2023'\''|show_to='\''1 July 2024'\''|' "${dir}/${dir}23_24.html"
+  # Update the archive line, title, layout, permalink, show_from and show_to for 2023-24 in the new file
+  sed -i '' -e 's|<a href="/seminars/'${dir}'/">upcoming</a> | <a href="/seminars/'${dir}'/2022-23/">2022-23</a>|<a href="/seminars/'${dir}'/">upcoming</a> | <a href="/seminars/'${dir}'/2023-24/">2023-24</a> | <a href="/seminars/'${dir}'/2022-23/">2022-23</a>|g' \
+  -e 's|2022-23|2023-24|g' \
+  -e 's|show_from='\''1 July 2022'\''|show_from='\''1 July 2023'\''|g' \
+  -e 's|show_to='\''1 July 2023'\''|show_to='\''1 July 2024'\''|g' "${dir}/${dir}23_24.html"
 done
