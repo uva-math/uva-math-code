8f98f5a99bb4034e4c39f660154220cb4b53d199
Leonid Petrov <lenia.petrov@gmail.com>
2024-06-14 09:29:55 -0700

    Update update_sems_year.zsh

--- seminars/update_sems_year.zsh
+++ seminars/update_sems_year.zsh
@@ -2,24 +2,24 @@
 
 # Iterate over the directories
 for dir in algebra colloq diffeq galois geometry gradsem mathclub mathphys ntsem probability sotoa topology; do
-  # Create the 2023-24 file
-  touch "${dir}/${dir}23_24.html"
+  # Create the 2024-25 file
+  touch "${dir}/${dir}24_25.html"
 
   # Copy content of the original file to the new one
-  cp "${dir}/${dir}22_23.html" "${dir}/${dir}23_24.html"
+  cp "${dir}/${dir}23_24.html" "${dir}/${dir}24_25.html"
 
-  # Update the archive line, title, layout, permalink, show_from and show_to for 2023-24 in the new file
+  # Update the archive line, title, layout, permalink, show_from and show_to for 2024-25 in the new file
   sed -i '' \
-  -e 's#2022-23/$#2023-24/#g' \
-  -e 's#2022-23$#2023-24#g' \
-  -e 's#show_from='\''1 July 2022'\''#show_from='\''1 July 2023'\''#g' \
-  -e 's#show_to='\''1 July 2023'\''#show_to='\''1 July 2024'\''#g' "${dir}/${dir}23_24.html"
+  -e 's#2023-24/$#2024-25/#g' \
+  -e 's#2023-24$#2024-25#g' \
+  -e 's#show_from='\''1 July 2023'\''#show_from='\''1 July 2024'\''#g' \
+  -e 's#show_to='\''1 July 2024'\''#show_to='\''1 July 2025'\''#g' "${dir}/${dir}24_25.html"
 
   for file in "${dir}"/*.html; do
-    sed -i '' -e "s#<a href=\"/seminars/${dir}/\">upcoming</a> | <a href=\"/seminars/${dir}/2022-23/\">2022-23</a>#<a href=\"/seminars/${dir}/\">upcoming</a> | <a href=\"/seminars/${dir}/2023-24/\">2023-24</a> | <a href=\"/seminars/${dir}/2022-23/\">2022-23</a>#g" "${file}"
+    sed -i '' -e "s#<a href=\"/seminars/${dir}/\">upcoming</a> | <a href=\"/seminars/${dir}/2023-24/\">2023-24</a>#<a href=\"/seminars/${dir}/\">upcoming</a> | <a href=\"/seminars/${dir}/2024-25/\">2024-25</a> | <a href=\"/seminars/${dir}/2023-24/\">2023-24</a>#g" "${file}"
     sed -i '' -e '/<a href="\/seminars\/'${dir}'\/">upcoming<\/a> |$/{
     N
-    s#<a href="\/seminars\/'${dir}'\/">upcoming<\/a> |\n<a href="\/seminars\/'${dir}'\/2022-23\/">2022-23<\/a>#<a href="\/seminars\/'${dir}'\/">upcoming<\/a> | <a href="\/seminars\/'${dir}'\/2023-24\/">2023-24<\/a> | <a href="\/seminars\/'${dir}'\/2022-23\/">2022-23<\/a>#g
+    s#<a href="\/seminars\/'${dir}'\/">upcoming<\/a> |\n<a href="\/seminars\/'${dir}'\/2023-24\/">2023-24<\/a>#<a href="\/seminars\/'${dir}'\/">upcoming<\/a> | <a href="\/seminars\/'${dir}'\/2024-25\/">2024-25<\/a> | <a href="\/seminars\/'${dir}'\/2023-24\/">2023-24<\/a>#g
   }' "${file}"
   done
 done
